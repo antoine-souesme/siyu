199e8fa98b83b6c3139f04b5c6d84eadc547cec9
Antoine Souesme <antoine@lapala.io>
2025-03-16 10:40:56 +0100

    Setup eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,75 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist', 'cypress.config.ts'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    { ignores: ['dist', 'cypress.config.ts'] },
+    {
+        extends: [js.configs.recommended, ...tseslint.configs.recommended],
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        plugins: {
+            'react': react,
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': [
+                'warn',
+                { allowConstantExport: true },
+            ],
+            'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+            'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+
+            "indent": [
+                "error",
+                4,
+                {
+                    "SwitchCase": 1,
+                },
+            ],
+            "semi": [
+                "error",
+                "always",
+            ],
+            "comma-dangle": [
+                "error",
+                {
+                    "arrays": "always-multiline",
+                    "objects": "always-multiline",
+                    "imports": "always-multiline",
+                    "exports": "always-multiline",
+                    "functions": "always-multiline",
+                },
+            ],
+
+            "react/jsx-max-props-per-line": [
+                "error",
+                {
+                    "maximum": 1,
+                },
+            ],
+            "react/jsx-curly-spacing": [
+                "error",
+                {
+                    "when": "always",
+                },
+            ],
+            'react/jsx-first-prop-new-line': [
+                'error',
+                'multiline',
+            ],
+            "react/jsx-closing-bracket-location": [
+                "error",
+                "line-aligned",
+            ],
+        },
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    },
-  },
 )
--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -3,12 +3,16 @@ import './ExploreContainer.css';
 interface ContainerProps { }
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
-  return (
-    <div id="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
-  );
+    return (
+        <div id="container">
+            <strong>Ready to create an app?</strong>
+            <p>Start with Ionic <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://ionicframework.com/docs/components"
+            >UI Components</a></p>
+        </div>
+    );
 };
 
 export default ExploreContainer;
