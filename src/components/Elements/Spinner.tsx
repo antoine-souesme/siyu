a15226960ec3228e74a4c62e1da214ae179314be
Antoine Souesme <antoine@lapala.io>
2025-03-18 15:01:43 +0100

    Cleaning

--- src/components/Elements/Spinner.tsx
+++ src/components/Elements/Spinner.tsx
@@ -42,6 +42,7 @@ export const Spinner = ({
             className={ clsx([
                 "mr-3 -ml-1 animate-spin",
                 `text-${color} size-${size}`,
+                className,
             ]) }
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
